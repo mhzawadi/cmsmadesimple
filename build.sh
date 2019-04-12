baa898fc6003a4a9e4b5c331e11ae833d763061e
Matthew Horwood <matt.horwood@mayden.co.uk>
2019-04-12 10:40:04 +0100

    fix up arm build

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@
 
 # generate the image tag
 #export TARGET_IMAGE_TAG=$(if [ "$TRAVIS_BRANCH" = "master" ]; then if [ "$TAG_SUFFIX" = "" ]; then echo "latest"; else echo "$TAG_SUFFIX"; fi; else if [ "$TAG_SUFFIX" = "" ]; then echo "$TRAVIS_BRANCH"; else echo "$TRAVIS_BRANCH-$TAG_SUFFIX"; fi; fi)
+if [ "`git branch | grep \* | cut -d ' ' -f2`" = "master" ]
+then
+  export TARGET_IMAGE_TAG="latest-${TAG_SUFFIX}"
+else
+  export TARGET_IMAGE_TAG="`git branch | grep \* | cut -d ' ' -f2`-${TAG_SUFFIX}"
+fi
 
 # pull the existing image from the registry, if it exists, to use as a build cache
 docker pull $SOURCE_IMAGE:$SOURCE_IMAGE_TAG && export IMAGE_CACHE="--cache-from $SOURCE_IMAGE:$SOURCE_IMAGE_TAG" || export IMAGE_CACHE=""
@@ -18,7 +24,13 @@ if [ -z "$ALT_SUFFIX" ]; then
   echo "No alternate tags set for this build.";
 else
   echo "Tagging with alternate tag '$ALT_SUFFIX'";
-  export ALT_IMAGE_TAG=$(if [ "$TRAVIS_BRANCH" = "master" ]; then if [ "$ALT_SUFFIX" = "" ]; then echo "error"; else echo "$ALT_SUFFIX"; fi; else if [ "$ALT_SUFFIX" = "" ]; then echo "$TRAVIS_BRANCH"; else echo "$TRAVIS_BRANCH-$ALT_SUFFIX"; fi; fi);
+  #export ALT_IMAGE_TAG=$(if [ "$TRAVIS_BRANCH" = "master" ]; then if [ "$ALT_SUFFIX" = "" ]; then echo "error"; else echo "$ALT_SUFFIX"; fi; else if [ "$ALT_SUFFIX" = "" ]; then echo "$TRAVIS_BRANCH"; else echo "$TRAVIS_BRANCH-$ALT_SUFFIX"; fi; fi);
+  if [ "`git branch | grep \* | cut -d ' ' -f2`" = "master" ]
+  then
+    export ALT_IMAGE_TAG="latest-${ALT_SUFFIX}"
+  else
+    export ALT_IMAGE_TAG="`git branch | grep \* | cut -d ' ' -f2`-${ALT_SUFFIX}"
+  fi
   docker tag $TARGET_IMAGE:$TARGET_IMAGE_TAG $TARGET_IMAGE:$ALT_IMAGE_TAG;
   docker push $TARGET_IMAGE:$ALT_IMAGE_TAG;
 fi
