cf3cc9051d6aadfd7b3e75b275e25d4f6117f8b0
Matthew Horwood <matt.horwood@mayden.co.uk>
2019-04-13 22:32:31 +0100

    - remove commnets fix remove installer

--- dist/usr/local/bin/entrypoint.sh
+++ dist/usr/local/bin/entrypoint.sh
@@ -5,28 +5,10 @@ cd /var/www/html
 
 . docker-php-entrypoint
 
-# chown -R www-data:www-data tmp/templates_c
-# chown -R www-data:www-data tmp/cache
-# chown -R www-data:www-data uploads
-# chown -R www-data:www-data uploads/images
-# chown -R www-data:www-data modules
-
-# cp /config.template.php /var/www/html/config.php
-# chown -R www-data:www-data /var/www/html/config.php
-# sed -i -e "s/###MYSQL_HOST###/${MYSQL_HOST}/g" /var/www/html/config.php
-# sed -i -e "s/###MYSQL_USER###/${MYSQL_USER}/g" /var/www/html/config.php
-# sed -i -e "s/###MYSQL_PASSWORD###/${MYSQL_PASSWORD}/g" /var/www/html/config.php
-# sed -i -e "s/###MYSQL_DATABASE###/${MYSQL_DATABASE}/g" /var/www/html/config.php
-
 if [[ -n "${REMOVE_INSTALL_FOLDER}" ]]
 then
 	echo "Removing install dir"
-	rm -rfv install
-	chmod 0640 /var/www/html/config.php
-else
-	echo "Making config.php writable"
-	touch /var/www/html/config.php
-	chmod 0666 /var/www/html/config.php
+	rm -rfv cmsms-${CMSMS_VERSION}-install.php
 fi
 
 exec apache2-foreground
