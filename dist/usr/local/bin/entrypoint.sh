4ceeb9b2e628e45ba3a7bb02b43789ce6388a3b8
Matthew Horwood <matt.horwood@mayden.co.uk>
2019-04-15 11:31:10 +0100

    minor config fix

--- dist/usr/local/bin/entrypoint.sh
+++ dist/usr/local/bin/entrypoint.sh
@@ -9,11 +9,11 @@ if [[ -n "${REMOVE_INSTALL_FOLDER}" ]]
 then
 	echo "Removing install dir"
 	rm -rfv cmsms-${CMSMS_VERSION}-install.php
-	chmod 0640 /var/www/html/config/config.php
+	chmod 0640 /var/www/html/config.php
 else
 	echo "Making config.php writable"
 	touch /var/www/html/config.php
-	chmod 0666 /var/www/html/config/config.php
+	chmod 0666 /var/www/html/config.php
 fi
 
 exec apache2-foreground
