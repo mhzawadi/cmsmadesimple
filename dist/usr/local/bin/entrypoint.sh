6a8166c9b8ee23fe30c49a14ecaf5185fe3d8b0f
Matthew Horwood <matt.horwood@mayden.co.uk>
2019-04-15 12:37:37 +0100

    add chown to config

--- dist/usr/local/bin/entrypoint.sh
+++ dist/usr/local/bin/entrypoint.sh
@@ -9,10 +9,12 @@ if [[ -n "${REMOVE_INSTALL_FOLDER}" ]]
 then
 	echo "Removing install dir"
 	rm -rfv cmsms-${CMSMS_VERSION}-install.php
+	chown -R www-data:www-data /var/www/html/config.php
 	chmod 0640 /var/www/html/config.php
 else
 	echo "Making config.php writable"
 	touch /var/www/html/config.php
+	chown -R www-data:www-data /var/www/html/config.php
 	chmod 0666 /var/www/html/config.php
 fi
 
